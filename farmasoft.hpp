bc86b12b57e931b8188cc720ac8ebf4a54c961a1
Enrique Alejo <enrique.m.alejo@gmail.com>
2019-11-19 16:05:29 +0100

    Ctransport finished

--- farmasoft.hpp
+++ farmasoft.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <time.h>
 #include <iostream>
+#include <list>
 #include "CDateTime.hpp"
 #include <string>
 using namespace std;
@@ -41,29 +42,46 @@ class CPos{
     float get_y();
 };
 
+//CTRANSPORT
 class CTransport{
 
     private:
       int m_i_idtransporte;
-      CDateTime m_dt_fecha;
+      CDateTime m_fecha;
       float m_f_distancia;
+      CVehiculo * m_vehiculo;
+      list<CMedicamento*> m_medicamentos;
+      CFarmacia* m_farmacia_origen;
+      CFarmacia* m_farmacia_destino;
+      int executed;
 
     public:
       CTransport(){
                 m_i_idtransporte=0;
-                m_dt_fecha=0;
+                m_fecha=0;
                 m_f_distancia=0;
+                m_medicamentos={};
+                m_farmacia_origen=NULL;
+                m_farmacia_destino=NULL;
+                executed=0;
       };
-      CTransport(int id,CDateTime fecha, float dist){
+      CTransport(int id,CDateTime fecha, float dist, CVehiculo* veh,list<CMedicamento*> meds,CFarmacia* origen,CFarmacia* destino){
                 m_i_idtransporte=id;
-                m_dt_fecha=fecha;
+                m_fecha=fecha;
                 m_f_distancia=dist;
+                m_vehiculo=veh;
+                m_medicamentos=meds;
+                m_farmacia_origen=origen;
+                m_farmacia_destino=destino;
+                executed=0;
       };
       ~CTransport(){};
+      int exec_transport(void);
             
-
-
 };
+
+
+
 
 
 
