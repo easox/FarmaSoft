151656bb6dc06e3a5d95d847913cdd4f8647668f
mikolfuentes <57764319+mikolfuentes@users.noreply.github.com>
2019-11-26 18:54:44 +0100

    CMedFarm.hpp modificado

--- CMedFarm.hpp
+++ CMedFarm.hpp
@@ -7,19 +7,26 @@
 
 using namespace std;
 
-class CMedFarm{
-  private:
-    int m_cantidad;
-    int m_minima;
-    CMedicamento* m_medicamento;
+class CMedFarm {
+private:
+	int m_i_cantidad;
+	int m_i_cantidad_min;
+	int m_i_cantidad_max;
+	CMedicamento* m_medicamento;
 
-  public:
-    CMedFarm(int cantida,int minima, CMedicamento* medicamento);
-    int get_cantidad(void);//TODO:
-    int get_cantidad_min(void);//TODO:
-    CMedicamento* get_med(void);//TODO:
+public:
+	CMedFarm(int cantidad, int cantidad_min, int cantidad_max, CMedicamento* medicamento);
+	CMedFarm(void);
+	int get_cantidad(void);
+	int get_cantidad_min(void);
+	int get_cantidad_max(void);
+	CMedicamento* get_med(void);
+
+	friend std::ostream& operator<<(std::ostream& os, const CMedFarm& medfarm);
+
+
+	~CMedFarm(void);
 
 
 
-
-}
+};
