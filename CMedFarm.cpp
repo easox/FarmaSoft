f475fb4ca970a41d38f07a5e7ba8b85fd7b5aa89
mikolfuentes <57764319+mikolfuentes@users.noreply.github.com>
2019-11-26 18:55:34 +0100

    CMedFarm.cpp Creada

---
+++ CMedFarm.cpp
@@ -0,0 +1,42 @@
+#include "CMedFarm.hpp"
+
+CMedFarm::CMedFarm(void) {
+	m_i_cantidad = -1;
+	m_i_cantidad_min = -1;
+	m_i_cantidad_max = -1;
+	m_medicamento = NULL;
+}
+CMedFarm::CMedFarm(int cantidad, int cantidad_min, int cantidad_max, CMedicamento* medicamento) {
+	m_i_cantidad = cantidad;
+	m_i_cantidad_min = cantidad_min;
+	m_i_cantidad_max = cantidad_max;
+	m_medicamento = medicamento;
+}
+int CMedFarm::get_cantidad(void) {
+	int cantidad = -1;
+	cantidad = this->m_i_cantidad;
+	return cantidad;
+}
+int CMedFarm::get_cantidad_min(void) {
+	int cantidad_min = -1;
+	cantidad_min = this->m_i_cantidad_min;
+	return cantidad_min;
+}
+int CMedFarm::get_cantidad_max(void) {
+	int cantidad_max = -1;
+	cantidad_max = this->m_i_cantidad_max;
+	return cantidad_max;
+}
+
+CMedicamento* CMedFarm::CMedFarm::get_med(void) {
+	return this->m_medicamento;
+}
+
+ostream& operator<<(std::ostream& os, const CMedFarm& medfarm) {
+	os << "(" << medfarm.m_i_cantidad << "," << medfarm.m_i_cantidad_min << "," << medfarm.m_i_cantidad_max<<"," << *medfarm.m_medicamento<< ")";
+	return os;
+}
+
+CMedFarm::~CMedFarm() {
+	cout << "CMedFarm has been deleted with = " << *this << endl;
+}
