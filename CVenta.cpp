22bc2cdd5664480bbcf010e6b19287eeff819c52
Enrique Alejo <enrique.m.alejo@gmail.com>
2019-11-26 19:48:52 +0100

    added cventa

---
+++ CVenta.cpp
@@ -0,0 +1,21 @@
+#include "CVenta.hpp"
+
+
+CVenta::CVenta(){
+  m_med_vendido=NULL;
+  m_fecha=new CDateTime(time(0));
+  m_cantidad=0;
+  
+
+};
+CVenta::CVenta(CMedicamento* med,int cantidad){
+  m_med_vendido=med;
+  m_fecha=new CDateTime(time(0));
+  m_cantidad=cantidad;
+};
+
+CVenta::~CVenta(){
+  free(m_med_vendido);
+  free(m_fecha);
+
+};
---
+++ CVenta.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+#include "CMedicamento.hpp"
+#include "CDateTime.hpp"
+
+class CVenta{
+  private:
+    CMedicamento* m_med_vendido;
+    CDateTime* m_fecha;
+    int m_cantidad;
+
+  public:
+
+    CVenta();
+    CVenta(CMedicamento* med,int cantidad);
+    ~CVenta();
+}
