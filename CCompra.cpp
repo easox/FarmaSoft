eb5539d30ea5e33f86cae53f373305ec413b63c6
Enrique Alejo <enrique.m.alejo@gmail.com>
2019-11-26 19:11:34 +0100

    Ccompra checked

---
+++ CCompra.cpp
@@ -0,0 +1,56 @@
+#include "CCompra.hpp"
+CCompra::CCompra(int id_compra, CFarmacia* to_farmacia,list<CMedCompra*>) {
+			
+  m_i_id_compra=id_compra;
+  m_farmacia=to_farmacia;
+  m_meds.clear(); //limpiar la lista de meds comprados
+  m_fecha=new CDateTime(time(0)); //set fecha to now
+
+};
+
+CCompra::CCompra(CFarmacia* to_farmacia,list<CMedCompra*> med_needed){
+  m_i_id_compra=0;
+  m_farmacia=to_farmacia;
+  m_meds=med_needed;
+  m_fecha=new CDateTime(time(0)); //set fecha to now
+}
+
+CCompra::~CCompra(){
+  free(m_fecha);
+  free(m_farmacia);
+
+  list<CMedCompra*>::iterator it;
+  while(!m_meds.empty()){
+    it=m_meds.begin();
+    m_meds.pop_front();
+    delete *it;
+  }
+}
+
+void CCompra::find_proveedor(list<CProveedor*> global_proveedores){
+  list<CMedCompra*>::iterator it_meds;
+  list<CProveedor*>::iterator it_prov;
+  
+  for(it_meds=m_meds.begin();it_meds != m_meds.end();++it_meds){
+    for(it_prov=global_proveedores.begin();it_prov != global_proveedores.end();++it_prov){
+      if((*it_prov)->med_available((*it_meds)->get_med())){
+        if((*it_meds)->get_price()>(*it_prov)->get_precio((*it_meds)->get_med())){
+
+          (*it_meds)->update_price((*it_prov)->get_precio((*it_meds)->get_med())); //update price
+          (*it_meds)->update_proveedor(*it_prov);//update proveedor
+          
+        }else{
+          
+        }
+        
+      }
+
+
+    }
+
+  }
+}
+
+int CCompra::get_id(void) { 
+  return this->m_i_id_compra;
+}
--- CCompra.hpp
+++ CCompra.hpp
@@ -19,51 +19,12 @@ class CCompra {
 
 	public:
 		CCompra(){};
-		CCompra(int id_compra, CFarmacia* to_farmacia,list<CMedCompra*>) {
-			
-			m_i_id_compra=id_compra;
-			m_farmacia=to_farmacia;
-			m_meds.clear(); //limpiar la lista de meds comprados
-			m_fecha=new CDateTime(time(0)); //set fecha to now
+		CCompra(int id_compra, CFarmacia* to_farmacia,list<CMedCompra*>);
+		CCompra(CFarmacia* to_farmacia,list<CMedCompra*> med_needed);
+		~CCompra();
 
-		 };
-		CCompra(CFarmacia* to_farmacia,list<CMedCompra*> med_needed){
-			m_farmacia=to_farmacia;
-			m_meds=med_needed;
-			m_fecha=new CDateTime(time(0)); //set fecha to now
-
-		}
-		~CCompra(){
-			free(m_fecha);
-			free(m_farmacia);
-		}
-
-		void find_proveedor(list<CProveedor*> global_proveedores){
-			list<CMedCompra*>::iterator it_meds;
-			list<CProveedor*>::iterator it_prov;
-			
-			for(it_meds=m_meds.begin();it_meds != m_meds.end();++it_meds){
-				for(it_prov=global_proveedores.begin();it_prov != global_proveedores.end();++it_prov){
-					if((*it_prov)->med_available((*it_meds)->get_med())){
-						if((*it_meds)->get_price()>(*it_prov)->get_precio((*it_meds)->get_med())){
-
-							(*it_meds)->update_price((*it_prov)->get_precio((*it_meds)->get_med())); //update price
-							(*it_meds)->update_proveedor(*it_prov);//update proveedor
-							
-						}else{
-							
-						}
-						
-					}
-
-
-				}
-
-			}
-		}
-
-
-		int get_id(void) { return this->m_i_id_compra;}
+		void find_proveedor(list<CProveedor*> global_proveedores);
+		int get_id(void);
 
 };
 
