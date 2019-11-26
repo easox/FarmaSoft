42b73fa848e883bc21a5bacf1b0934eb4ef542fd
Enrique Alejo <enrique.m.alejo@gmail.com>
2019-11-26 18:44:58 +0100

    cambios a cfarmacia

---
+++ CFarmacia.cpp
@@ -0,0 +1,45 @@
+#include "CFarmacia.hpp"
+
+CFarmacia::CFarmacia(){
+      m_i_id_farmacia=0;
+      m_pos=NULL;
+      m_medicamentos.clear();
+    };
+
+CFarmacia::CFarmacia(int id, CPos* pos,list<CMedFarm*> medicamentos){
+      m_i_id_farmacia=id;
+      m_pos=pos;
+      m_medicamentos=medicamentos;
+    };
+CFarmacia::~CFarmacia(){
+  free(m_pos);
+
+  list<CMedFarm*>::iterator it;
+  while(!m_medicamentos.empty()){
+    it=m_medicamentos.begin();
+    m_medicamentos.pop_front();
+    delete *it;
+  }
+  
+}
+
+CCompra* CFarmacia::determine_compra(){ 
+    
+      
+      list<CMedCompra*> med_needed;
+      list<CMedFarm*>::iterator it_farm;
+      list<CMedCompra*>::iterator it_meds;
+
+      for(it_farm=this->m_medicamentos.begin();it_farm != this->m_medicamentos.end();++it_farm){
+        if((*it_farm)->get_cantidad()<=(*it_farm)->get_cantidad_min()+BUFFER){
+
+          CMedCompra* temp_med_compra=new CMedCompra((*it_farm)->get_med(),(*it_farm)->get_cantidad_max()-(*it_farm)->get_cantidad());
+
+        }
+      }
+
+
+      CCompra* compra=new CCompra(this,med_needed);
+      return compra;
+      
+};
--- CFarmacia.hpp
+++ CFarmacia.hpp
@@ -6,6 +6,7 @@
 #include "CPos.hpp"
 #include "CMedFarm.hpp"
 #include "CMedCompra.hpp"
+#include "CCompra.hpp"
 
 #define BUFFER 100
 
@@ -15,42 +16,12 @@ using namespace std;
 class CFarmacia{
   private:
     int m_i_id_farmacia;
-    int m_i_capacidad_max;
     CPos* m_pos;
     list<CMedFarm*> m_medicamentos;
 
   public:
-    CFarmacia(){
-      m_i_id_farmacia=0;
-      m_i_capacidad_max=0;
-      m_pos=NULL;
-      m_medicamentos.clear();
-    };
-    CFarmacia(int id, int capacidad, CPos* pos,list<CMedFarm*> medicamentos){
-      m_i_id_farmacia=id;
-      m_i_capacidad_max=capacidad;
-      m_pos=pos;
-      m_medicamentos=medicamentos;
-    };
-
-    ~CFarmacia(){};
-    CCompra* determine_compra(){ //returns what it needs to buy(quantity) but not from whom or at what price.
-    
-      
-      list<CMedCompra*> med_needed;
-      list<CMedFarm*>::iterator it_farm;
-      list<CMedCompra*>::iterator it_meds;
-
-      for(it_farm=this->m_medicamentos.begin();it_farm != this->m_medicamentos.end();++it_farm){
-        if((*it_farm)->get_cantidad()<=(*it_farm)->get_cantidad_min()+BUFFER){
-          CMedCompra* temp_med_compra=new CMedCompra((*it_farm)->get_med(),TODO:);// Define quantity asked
-
-        }
-      }
-
-
-      CCompra* compra=new CCompra(this,med_needed);
-      return compra;
-      
-    };
+    CFarmacia();
+    CFarmacia(int id, CPos* pos,list<CMedFarm*> medicamentos);
+    ~CFarmacia();
+    CCompra* determine_compra();//returns what it needs to buy(quantity) but not from whom or at what price.
 };
