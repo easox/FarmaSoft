13090d9826f174eb2b770d2a3248321056c3f476
Enrique Alejo <enrique.m.alejo@gmail.com>
2019-12-03 21:27:09 +0100

    resolved merge ccompra

--- CCompra.hpp
+++ CCompra.hpp
@@ -28,12 +28,8 @@ class CCompra {
 		~CCompra();
 
 		void find_proveedor(list<CProveedor*> global_proveedores);
-<<<<<<< HEAD
 		int get_id(void);
     friend ostream& operator<<(std::ostream& o, CCompra* compra);
-=======
-        friend ostream& operator<<(std::ostream& o, CCompra* compra);
->>>>>>> b1db800fe190d0ef1ec0e0e99dc21235c5ac5d19
 
 		list<CMedCompra*> getMedCompra(void) { return m_meds; };
 		CDateTime* getFecha(void) { return m_fecha; };
